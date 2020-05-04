7bdac56960d38ecec514598c6ec5929fd318ca48
laylatame <laylatame@Laylas-MacBook-Pro.local>
2020-05-04 12:25:39 -0500

    Start final project

---
+++ app/src/main/java/com/example/culturavisual/Cuestionarios.java
@@ -0,0 +1,14 @@
+package com.example.culturavisual;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Cuestionarios extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cuestionarios);
+    }
+}
